3edffb9696ec54eb3cf377c9e402cb4da9bc5a38
Edricpw <117250587+Edricpw@users.noreply.github.com>
2022-11-11 16:19:09 +0700

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ import {BsWhatsapp} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'> 
-    <a href='www.linkedin.com/IN/edricpratamaw' targets="_blank"><BsLinkedin/></a>
+    <a href='https://www.linkedin.com/in/edricpratamaw/?originalSubdomain=id' targets="_blank"><BsLinkedin/></a>
     <a href='https://github.com' targets="_blank"><FaGithub/></a>
     <a href='https://wa.me/6281355161002' targets="_blank"><BsWhatsapp/></a>
     </div>
